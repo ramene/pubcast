6e149499b93ecc688e699312b14ae0f6a1ea1020
Ramene Anthony <ramene.anthony@live.com>
2018-11-09 18:46:58 -0500

    more housecleaning

--- data/data.go
+++ data/data.go
@@ -36,13 +36,13 @@ type ActivityPub struct {
 }
 
 // GetPool is a safer interface for accessing the Pool
-// func GetPool() (t dbPool, error)  {
-// 	if t.Db == nil {
-// 		panic("GetPool was used before the dbPool was defined.")
-// 	}
-	
-// 	return t.Db
-// }
+func GetPool() (s *dbPool, error)  {
+	if r.Db == nil {
+		panic("GetPool was used before the dbPool was defined.")
+	}
+
+	return
+}
 
 // NewDB opens a standard DB
 func NewDB() (*sql.DB, error) {
